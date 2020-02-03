f92519be0c3512a6da01d7798ac462db33851d64
chason <chasonlsmith@gmail.com>
2020-02-03 16:18:09 -0700

    fixed schema

--- oldCode/database/schema.sql
+++ oldCode/database/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(42) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 
 );
